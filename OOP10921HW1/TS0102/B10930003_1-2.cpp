89e4c6bd20222949b426a70f95b28db7450d20e4
weininglee <74503201+weininglee@users.noreply.github.com>
2021-02-25 08:41:46 +0800

    Update B10930003_1-2.cpp

--- OOP10921HW1/TS0102/B10930003_1-2.cpp
+++ OOP10921HW1/TS0102/B10930003_1-2.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main() {
-	long long int x, y, z; // x = hour , y = 
-	cin >> z;
-	y = z / 60;
-	x = y / 60;
-	y = y % 60;
-	z = z % 60;
-	cout << x << " hours " << y << " minutes and " << z << " seconds ";
+	unsigned long long int x, y, z; // x = hour , y = min , z = sec
+	while (cin >> z) {
+		y = z / 60;
+		x = y / 60;
+		y = y % 60;
+		z = z % 60;
+		cout << x << " hours " << y << " minutes and " << z << " seconds" << endl;
+	}
 }
