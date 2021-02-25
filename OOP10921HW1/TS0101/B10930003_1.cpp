5f8133b67732b882cfe2f62965905e7ba36341c4
weininglee <74503201+weininglee@users.noreply.github.com>
2021-02-25 08:41:11 +0800

    Update B10930003_1.cpp

--- OOP10921HW1/TS0101/B10930003_1.cpp
+++ OOP10921HW1/TS0101/B10930003_1.cpp
@@ -5,14 +5,15 @@ using namespace std;
 
 int main() {
 	double n, r, guess;
-	cin >> n;
-	guess = n / 2;
-	r = n / guess;
-	guess = (guess + r) / 2;
-	do {
+	while (cin >> n) {
+		guess = n / 2;
 		r = n / guess;
 		guess = (guess + r) / 2;
-	} while (guess - r > 0.01);
-	/*cout << setprecision(2) << guess;*/
-	cout << setprecision(2) << setiosflags(ios::fixed) << guess;
+		do {
+			r = n / guess;
+			guess = (guess + r) / 2;
+		} while (guess - r > 0.01);
+		/*cout << setprecision(2) << guess;*/
+		cout << setprecision(2) << setiosflags(ios::fixed) << guess << endl;
+	}
 }
